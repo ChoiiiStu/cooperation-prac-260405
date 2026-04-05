e1de6a137b5c48a017d6c9a1180265a724c4aad3
JunChoi_B <perandowind@gmail.com>
2026-04-05 21:09:30 +0900

    15,16,17 로그인 구현 및 테스트 케이스(성공, 실패) 작성

--- src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
@@ -3,6 +3,7 @@ package com.back.domain.member.controller;
 import com.back.domain.member.dto.MemberDto;
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.service.MemberService;
+import com.back.global.exception.ServiceException;
 import com.back.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -24,16 +25,25 @@ public class ApiV1MemberController {
             String password,
             String nickname
 
-    ){}
+    ) {
+    }
 
     public record MemberJoinResBody(
             MemberDto memberDto
-    ){}
+    ) {
+    }
 
-    @PostMapping
+    @PostMapping("/join")
     @Operation(summary = "회원가입")
     public RsData<MemberJoinResBody> join(@RequestBody @Valid MemberJoinReqBody reqBody) {
-        Member member = memberService.join(reqBody.username, reqBody.password(), reqBody.nickname());
+
+        memberService.findByUsername(reqBody.username).ifPresent(
+                m -> {
+                    throw new ServiceException("409-1", "이미 사용중인 아이디입니다.");
+                }
+        );
+
+        Member member = memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
 
         return new RsData<>(
                 "회원가입이 완료되었습니다. %s님 환영합니다.".formatted(reqBody.nickname),
@@ -44,4 +54,35 @@ public class ApiV1MemberController {
         );
     }
 
+    public record MemberLoginReqBody(
+            String username,
+            String password,
+            String nickname
+
+    ) {
+    }
+
+    public record MemberLoginResBody(
+            String apiKey
+    ) {
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "로그인")
+    public RsData<MemberLoginResBody> login(@RequestBody @Valid MemberLoginReqBody reqBody) {
+
+        Member actor = memberService.findByUsername(reqBody.username).orElseThrow(
+                () -> new ServiceException("401-1", "존재하지 않는 아이디입니다.")
+        );
+
+        if (!actor.getPassword().equals(reqBody.password)) {
+            throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
+        }
+
+        return new RsData<>(
+                "%s님 환영합니다.".formatted(actor.getName()),
+                "200-1",
+                new MemberLoginResBody(actor.getApiKey())
+        );
+    }
 }
