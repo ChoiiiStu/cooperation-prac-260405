f477249749e028a293ac3b6c2fecd008a7c23989
JunChoi_B <perandowind@gmail.com>
2026-04-05 22:51:50 +0900

    41, 42 쿠키 생성 로직 구현 by TDD

--- src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
@@ -7,6 +7,8 @@ import com.back.global.exception.ServiceException;
 import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,7 @@ public class ApiV1MemberController {
 
     private final MemberService memberService;
     private final Rq rq;
+    private final HttpServletResponse response;
 
     public record MemberJoinReqBody(
             String username,
@@ -77,6 +80,10 @@ public class ApiV1MemberController {
         if (!actor.getPassword().equals(reqBody.password)) {
             throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
         }
+
+        response.addCookie(
+                new Cookie("apiKey", actor.getApiKey())
+        );
 
         return new RsData<>(
                 "%s님 환영합니다.".formatted(actor.getName()),
