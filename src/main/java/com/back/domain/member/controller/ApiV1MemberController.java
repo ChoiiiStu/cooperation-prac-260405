bdb6f4c0828e7564184f20cafd4d8d9488799641
JunChoi_B <perandowind@gmail.com>
2026-04-07 01:29:24 +0900

    60-로그인 시 엑세스토큰 발급

--- src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
@@ -64,7 +64,8 @@ public class ApiV1MemberController {
     }
 
     public record MemberLoginResBody(
-            String apiKey
+            String apiKey,
+            String accessToken
     ) {
     }
 
@@ -80,13 +81,18 @@ public class ApiV1MemberController {
             throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
         }
 
-
         rq.addCookie("apiKey", actor.getApiKey());
+
+        String accessToken = memberService.genAccessToken(actor);
+        rq.addCookie("accessToken", accessToken);
 
         return new RsData<>(
                 "%s님 환영합니다.".formatted(actor.getName()),
                 "200-1",
-                new MemberLoginResBody(actor.getApiKey())
+                new MemberLoginResBody(
+                        actor.getApiKey(),
+                        accessToken
+                )
         );
     }
 
--- src/main/java/com/back/domain/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/service/MemberService.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final AuthTokenService authTokenService;
 
     public Member join(String username, String password, String nickname) {
         return join(username, password, nickname, UUID.randomUUID().toString());
@@ -34,5 +35,9 @@ public class MemberService {
 
     public Optional<Member> findByApiKey(String apiKey) {
         return memberRepository.findByApiKey(apiKey);
+    }
+
+    public String genAccessToken(Member member) {
+        return authTokenService.genAccessToken(member);
     }
 }
