2156f5ea4b8cb2b5029e2207b4e18e388c6ca149
JunChoi_B <perandowind@gmail.com>
2026-04-07 00:41:57 +0900

    50-55

--- src/main/java/com/back/domain/member/service/AuthTokenService.java
+++ src/main/java/com/back/domain/member/service/AuthTokenService.java
@@ -1,4 +1,25 @@
 package com.back.domain.member.service;
 
+import com.back.domain.member.entity.Member;
+import com.back.standard.ut.Ut;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
 public class AuthTokenService {
+
+    private String secretPattern = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+    private long expireSeconds = 1000L * 60 * 60 * 24 * 365; // 1년
+
+    public String genAccessToken(Member member) {
+        return Ut.jwt.toString(
+                secretPattern,
+                expireSeconds,
+                Map.of(
+                        "id", member.getId(),
+                        "name", member.getName()
+                )
+        );
+    }
 }
